3366b9f8755b0471c8e7f7c7c0090d9505c8676d
prajapathimanshu621-oss <prajapathimanshu621@gmail.com>
2026-03-13 10:43:50 +0530

    Refactor cake generation and cart display logic

--- script.js
+++ script.js
@@ -1,50 +1,88 @@
 let cakes=[]
-for(let i=1;i<=120;i++){
-cakes.push({name:"Cake "+i,price:200+i})
+
+// 200 cakes generate
+for(let i=1;i<=200;i++){
+
+cakes.push({
+name:"Cake "+i,
+price:200+i,
+image:"https://picsum.photos/200?random="+i
+})
+
 }
 
 let cart=[]
 
 function displayCakes(){
+
 let list=document.getElementById("cake-list")
+
 cakes.forEach((cake,index)=>{
+
 let div=document.createElement("div")
+
 div.className="cake"
 
 div.innerHTML=`
+
+<img src="${cake.image}">
+
 <h3>${cake.name}</h3>
+
 <p>₹${cake.price}</p>
-<button onclick="addToCart(${index})">Add to Cart</button>
+
+<button onclick="addToCart(${index})">
+Add to Cart
+</button>
+
 `
 
 list.appendChild(div)
+
 })
+
 }
 
 function addToCart(index){
+
 cart.push(cakes[index])
+
 updateCart()
+
 }
 
 function removeFromCart(index){
+
 cart.splice(index,1)
+
 updateCart()
+
 }
 
 function updateCart(){
+
 let cartDiv=document.getElementById("cart")
+
 cartDiv.innerHTML=""
 
 cart.forEach((item,i)=>{
+
 let div=document.createElement("div")
 
 div.innerHTML=`
+
 ${item.name} - ₹${item.price}
-<button onclick="removeFromCart(${i})">Remove</button>
+
+<button onclick="removeFromCart(${i})">
+Remove
+</button>
+
 `
 
 cartDiv.appendChild(div)
+
 })
+
 }
 
 displayCakes()
